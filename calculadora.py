dba6a1e9f3b47ffe985f313108c304ffd6b96c78
LautaroToloza <97970473+LautaroToloza@users.noreply.github.com>
2022-10-02 22:47:41 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -23,123 +23,67 @@ class Calculadora:
 
         # Botones
         # Fila 1
-        self.boton_7 = tk.Button(self.ventana, text="7", width=3, command=self.op_7)
-        self.boton_7.grid(column=0, row=1)
-        self.boton_7.configure(foreground="blue")
-        self.boton_8 = tk.Button(self.ventana, text="8", width=3, command=self.op_8)
-        self.boton_8.grid(column=1, row=1)
-        self.boton_8.configure(foreground="blue")
-        self.boton_9 = tk.Button(self.ventana, text="9", width=3, command=self.op_9)
-        self.boton_9.grid(column=2, row=1)
-        self.boton_9.configure(foreground="blue")
+        self.boton_7 = self.boton_num_punto("7", 0, 1)
+        self.boton_8 = self.boton_num_punto("8", 1, 1)
+        self.boton_9 = self.boton_num_punto("9", 2, 1)
         self.boton_borrar = tk.Button(self.ventana, text="BR", width=3, command=self.borrar)
         self.boton_borrar.grid(column=3, row=1)
         self.boton_borrar.configure(background="black")
         self.boton_borrar.configure(foreground="red")
         # Fila 2
-        self.boton_4 = tk.Button(self.ventana, text="4", width=3, command=self.op_4)
-        self.boton_4.grid(column=0, row=2)
-        self.boton_4.configure(foreground="blue")
-        self.boton_5 = tk.Button(self.ventana, text="5", width=3, command=self.op_5)
-        self.boton_5.grid(column=1, row=2)
-        self.boton_5.configure(foreground="blue")
-        self.boton_6 = tk.Button(self.ventana, text="6", width=3, command=self.op_6)
-        self.boton_6.grid(column=2, row=2)
-        self.boton_6.configure(foreground="blue")
-        self.boton_dividir = tk.Button(self.ventana, text="/", width=3, command=self.op_dividir)
-        self.boton_dividir.grid(column=3, row=2)
-        self.boton_dividir.configure(background="black")
-        self.boton_dividir.configure(foreground="white")
+        self.boton_4 = self.boton_num_punto("4", 0, 2)
+        self.boton_5 = self.boton_num_punto("5", 1, 2)
+        self.boton_6 = self.boton_num_punto("6", 2, 2)
+        self.boton_dividir = self.boton_operador("/", 3, 2)
         # Fila 3
-        self.boton_1 = tk.Button(self.ventana, text="1", width=3, command=self.op_1)
-        self.boton_1.grid(column=0, row=3)
-        self.boton_1.configure(foreground="blue")
-        self.boton_2 = tk.Button(self.ventana, text="2", width=3, command=self.op_2)
-        self.boton_2.grid(column=1, row=3)
-        self.boton_2.configure(foreground="blue")
-        self.boton_3 = tk.Button(self.ventana, text="3", width=3, command=self.op_3)
-        self.boton_3.grid(column=2, row=3)
-        self.boton_3.configure(foreground="blue")
-        self.boton_multiplicar = tk.Button(self.ventana, text="*", width=3, command=self.op_multiplicar)
-        self.boton_multiplicar.grid(column=3, row=3)
-        self.boton_multiplicar.configure(background="black")
-        self.boton_multiplicar.configure(foreground="white")
+        self.boton_1 = self.boton_num_punto("1", 0, 3)
+        self.boton_2 = self.boton_num_punto("2", 1, 3)
+        self.boton_3 = self.boton_num_punto("3", 2, 3)
+        self.boton_multiplicar = self.boton_operador("*", 3, 3)
         # Fila 4
-        self.boton_punto = tk.Button(self.ventana, text=".", width=3, command=self.op_punto)
-        self.boton_punto.grid(column=0, row=4)
-        self.boton_punto.configure(foreground="blue")
-        self.boton_0 = tk.Button(self.ventana, text="0", width=3, command=self.op_0)
-        self.boton_0.grid(column=1, row=4)
-        self.boton_0.configure(foreground="blue")
-        self.boton_mas = tk.Button(self.ventana, text="+", width=3, command=self.op_mas)
-        self.boton_mas.grid(column=2, row=4)
-        self.boton_mas.configure(background="black")
-        self.boton_mas.configure(foreground="white")
-        self.boton_menos = tk.Button(self.ventana, text="-", width=3, command=self.op_menos)
-        self.boton_menos.grid(column=3, row=4)
-        self.boton_menos.configure(background="black")
-        self.boton_menos.configure(foreground="white")
+        self.boton_punto = self.boton_num_punto(".", 0, 4)
+        self.boton_0 = self.boton_num_punto("0", 1, 4)
+        self.boton_mas = self.boton_operador("+", 2, 4)
+        self.boton_menos = self.boton_operador("-", 3, 4)
         # Fila 5
         self.boton_igual = tk.Button(self.ventana, text="=", width=3, command=self.op_igual)
         self.boton_igual.grid(column=2, row=5)
         self.boton_igual.configure(background="black")
         self.boton_igual.configure(foreground="white")
-        self.boton_potencia = tk.Button(self.ventana, text="^", width=3, command=self.op_potencia)
-        self.boton_potencia.grid(column=3, row=5)
-        self.boton_potencia.configure(background="black")
-        self.boton_potencia.configure(foreground="white")
+        self.boton_potencia = self.boton_operador("^", 3, 5)
         self.ventana.mainloop()
 
+    def boton_num_punto(self, valor, columna, fila):
+        self.boton = tk.Button(self.ventana, text=valor, width=3, command=lambda: self.op_numero(valor))
+        self.boton.grid(column=columna, row=fila)
+        self.boton.configure(foreground="blue")
+        return self.boton
+
+    def boton_operador(self, valor, columna, fila):
+        self.boton_op = tk.Button(self.ventana, text=valor, width=3, command=lambda: self.op_operador(valor))
+        self.boton_op.grid(column=columna, row=fila)
+        self.boton_op.configure(background="black")
+        self.boton_op.configure(foreground="white")
+        return self.boton_op
+
     # Métodos para el command
-    def op_0(self):
+    def op_numero(self, valor):
         self.nueva_operacion()
-        self.datos += "0"
+        self.datos += valor
         self.label.configure(text=self.datos)
 
-    def op_1(self):
-        self.nueva_operacion()
-        self.datos += "1"
-        self.label.configure(text=self.datos)
-
-    def op_2(self):
-        self.nueva_operacion()
-        self.datos += "2"
-        self.label.configure(text=self.datos)
-
-    def op_3(self):
-        self.nueva_operacion()
-        self.datos += "3"
-        self.label.configure(text=self.datos)
-
-    def op_4(self):
-        self.nueva_operacion()
-        self.datos += "4"
-        self.label.configure(text=self.datos)
-
-    def op_5(self):
-        self.nueva_operacion()
-        self.datos += "5"
-        self.label.configure(text=self.datos)
-
-    def op_6(self):
-        self.nueva_operacion()
-        self.datos += "6"
-        self.label.configure(text=self.datos)
-
-    def op_7(self):
-        self.nueva_operacion()
-        self.datos += "7"
-        self.label.configure(text=self.datos)
-
-    def op_8(self):
-        self.nueva_operacion()
-        self.datos += "8"
-        self.label.configure(text=self.datos)
-
-    def op_9(self):
-        self.nueva_operacion()
-        self.datos += "9"
-        self.label.configure(text=self.datos)
+    def op_operador(self, simbolo):
+        n = len(self.datos)
+        if n == 0 and simbolo == "-":
+            self.datos += simbolo
+            self.label.configure(text=self.datos)
+        else:
+            if n > 0:
+                if self.datos[n - 1] in self.operadores:
+                    pass
+                else:
+                    self.datos += simbolo
+                    self.label.configure(text=self.datos)
 
     def borrar(self):
         lista_borrar = []
@@ -156,61 +100,6 @@ class Calculadora:
 
         else:
             pass
-
-    # Operadores
-    def op_punto(self):
-        n = len(self.datos) - 1
-        if n >= 0:
-            if self.datos[n] in self.operadores:
-                pass
-            else:
-                self.datos += "."
-                self.label.configure(text=self.datos)
-
-    def op_mas(self):
-        n = len(self.datos) - 1
-        if n >= 0:
-            if self.datos[n] in self.operadores:
-                pass
-            else:
-                self.datos += "+"
-                self.label.configure(text=self.datos)
-
-    def op_menos(self):
-        n = len(self.datos) - 1
-        if n >= 0:
-            if self.datos[n] in self.operadores:
-                pass
-            else:
-                self.datos += "-"
-                self.label.configure(text=self.datos)
-
-    def op_multiplicar(self):
-        n = len(self.datos) - 1
-        if n >= 0:
-            if self.datos[n] in self.operadores:
-                pass
-            else:
-                self.datos += "*"
-                self.label.configure(text=self.datos)
-
-    def op_dividir(self):
-        n = len(self.datos) - 1
-        if n >= 0:
-            if self.datos[n] in self.operadores:
-                pass
-            else:
-                self.datos += "/"
-                self.label.configure(text=self.datos)
-
-    def op_potencia(self):
-        n = len(self.datos) - 1
-        if n >= 0:
-            if self.datos[n] in self.operadores:
-                pass
-            else:
-                self.datos += "^"
-                self.label.configure(text=self.datos)
 
     def op_igual(self):
         n = len(self.datos) - 1
@@ -235,30 +124,29 @@ class Calculadora:
 
     # Métodos para calcular resultado
     def agregar_num(self):
-        self.__num = "0123456789"
-        self.__numStr = ""
+        self.num = "0123456789."
         self.es_operador = self.cantidad_operadores * [False]
-        for car in self.datos:
-            if car in self.__num or car == ".":
-                self.__numStr += car
-                if car == ".":
+        self.__numStr = ""
+        for i in range(len(self.datos)):
+            if i == 0 and self.datos[i] == "-" or self.datos[i] in self.num:
+                self.__numStr += self.datos[i]
+                if self.datos[i] == ".":
                     self.hay_punto = True
             else:
                 if self.hay_punto is True:
                     self.lista.append(float(self.__numStr))
                 else:
                     self.lista.append(int(self.__numStr))
-                if car == "+":
+                if self.datos[i] == "+":
                     self.es_operador[0] = True
-                elif car == "-":
+                elif self.datos[i] == "-":
                     self.es_operador[1] = True
-                elif car == "*":
+                elif self.datos[i] == "*":
                     self.es_operador[2] = True
-                elif car == "/":
+                elif self.datos[i] == "/":
                     self.es_operador[3] = True
-                elif car == "^":
+                elif self.datos[i] == "^":
                     self.es_operador[4] = True
-
                 self.__numStr = ""
 
     def operacion(self):
